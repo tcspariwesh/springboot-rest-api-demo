838562ca43064e50b404dc9184db185272981a0b
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-24 13:21:53 +0530

    added database

--- src/main/java/com/tcs/springbootdemo/User.java
+++ src/main/java/com/tcs/springbootdemo/User.java
@@ -1,6 +1,16 @@
 package com.tcs.springbootdemo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {//not singleton
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String firstName;
 
 	public String getFirstName() {
