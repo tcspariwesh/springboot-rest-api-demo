0119e9ce09d8d7682eac54e208a6c6016cb338a6
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-24 12:41:48 +0530

    first commit

---
+++ src/main/java/com/tcs/springbootdemo/User.java
@@ -0,0 +1,13 @@
+package com.tcs.springbootdemo;
+
+public class User {//not singleton
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
