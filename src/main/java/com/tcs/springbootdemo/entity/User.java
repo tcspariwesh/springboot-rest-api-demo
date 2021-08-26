a99a3ad102d53ed3d7585bcb03e6c243d10c644d
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-26 11:10:37 +0530

    added Lombok

--- src/main/java/com/tcs/springbootdemo/entity/User.java
+++ src/main/java/com/tcs/springbootdemo/entity/User.java
@@ -6,30 +6,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class User {//not singleton
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@Email
 	private String email;
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	
 	@NotBlank
 	private String firstName;
-
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
 }
